1cb8845b2ab3f353d6bc31e825d894b7ad72f284
Don-Quixote-dev <suporte@luckysoft.com.br>
2025-09-09 15:12:44 -0400

    Update get-audio.php

--- get-audio.php
+++ get-audio.php
@@ -1,5 +1,3 @@
-analise o script abaixo, está dando problema de "timeout ao extrair o áudio":
-
 <?php
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
